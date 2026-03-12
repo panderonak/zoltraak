427f57d2b68e1284d2bfb75878ee01f958ecd585
panderonak <pandeyronak139@gmail.com>
2026-03-12 20:18:26 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@zoltraak/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
